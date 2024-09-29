e9c54490c29ee1dcf74d83cec6510bf1235877eb
skq <52236676+lumfa@users.noreply.github.com>
2024-09-29 15:27:52 +0300

    Update ElectricCar.java

--- pr3/vehicles/ElectricCar.java
+++ pr3/vehicles/ElectricCar.java
@@ -1,1 +1,23 @@
-
+public class ElectricCar extends Car
+{   
+  private int batteryCapacity;
+  public ElectricCar() {
+    super();        
+    this.engineType = "Electric";
+    this.batteryCapacity = 0;    
+  }
+    
+  public ElectricCar(String ownerName, int insuranceNumber, int batteryCapacity) {    
+    super(ownerName, insuranceNumber, "Electric");        
+    this.batteryCapacity = batteryCapacity;
+  }
+  
+  public int getBatteryCapacity() { return this.batteryCapacity; }
+  
+  public void setBatteryCapacity(int batteryCapacity) { this.batteryCapacity = batteryCapacity; }
+    
+  @Override    
+  public void To_String() {    
+    System.out.printf("%s %s %d %s %d %s %d %s\n", getColor(), getLicense(), getYear(), getModel(), getBatteryCapacity(), getOwnerName(), getInsuranceNumber(), getEngineType()); 
+  }
+}
