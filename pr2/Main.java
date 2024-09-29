86b32027e6e058b65a03a463ce373c0c3cfdab0f
skq <52236676+lumfa@users.noreply.github.com>
2024-09-29 15:15:43 +0300

    Update Main.java

--- pr2/Main.java
+++ pr2/Main.java
@@ -1,1 +1,25 @@
 
+public class Main
+{
+ public static void main(String[] args) {
+     
+     Car emptyObj = new Car();
+     Car firstCarObj = new Car("mustang", "am777r", "red", 2020);
+     Car secondObj = new Car("granta", 2018);
+     
+     System.out.println(firstCarObj.getColor());
+     
+     firstCarObj.setColor("black");
+     System.out.println(firstCarObj.getColor());
+     
+     firstCarObj.To_String();
+     
+     System.out.println(firstCarObj.carYearCalc());
+     
+     System.out.println("\n");
+     
+     secondObj.To_String();
+
+  System.out.println("Hello World");
+ }
+}
