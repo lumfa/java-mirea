a37cfb753f711190f08e151e47a9a3c6dc78aa09
skq <52236676+lumfa@users.noreply.github.com>
2024-10-03 17:44:11 +0300

    Create ElectricCar.java

---
+++ pr5/vehicle/ElectricCar.java
@@ -0,0 +1,53 @@
+package org.example.vehicles;
+
+public class ElectricCar extends Car implements ElectricVehicle {
+    public double batteryCapacity;
+
+    public ElectricCar(String model,
+                       String license,
+                       String color,
+                       int year,
+                       String ownerName,
+                       int insuranceNumber,
+                       double batteryCapacity) {
+        super(model, license, color, year, ownerName, insuranceNumber);
+        this.batteryCapacity = batteryCapacity;
+        setEngineType("Electric");
+    }
+
+    public ElectricCar() {}
+
+    @Override
+    public String vehicleType() {
+        return "ElectricCar";
+    }
+
+    public double getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    @Override
+    public void setBatteryCapacity(double batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    @Override
+    public void To_String() {
+        System.out.printf("Color: %s " +
+                        "\nLicense: %s " +
+                        "\nYear: %d " +
+                        "\nModel: %s " +
+                        "\nInsurance number: %d " +
+                        "\nOwner name: %s " +
+                        "\nEngine type: %s " +
+                        "\nBattery capacity: %,.3f\n\n",
+                getColor(),
+                getLicense(),
+                getYear(),
+                getModel(),
+                getInsuranceNumber(),
+                getOwnerName(),
+                getEngineType(),
+                getBatteryCapacity());
+    }
+}
